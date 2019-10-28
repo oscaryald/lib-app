0e10e1b81e9506dc385f4c6f45285c2b8465b807
Oleksandr Trokhymenko <Oleksandr_Trokhymenko@epam.com>
2019-10-28 13:02:17 +0200

    upgrde v lib

--- projects/shared-lib/src/lib/list/list.module.ts
+++ projects/shared-lib/src/lib/list/list.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ListComponent } from './list/list.component';
+import { ListComponent } from './list-container/list.component';
 
 @NgModule({
   declarations: [ListComponent],
--- projects/shared-lib/src/public-api.ts
+++ projects/shared-lib/src/public-api.ts
@@ -2,4 +2,4 @@ export * from './lib/list.service';
 export * from './lib/border/border.module';
 export * from './lib/border/border.directive';
 export * from './lib/list/list.module';
-export * from './lib/list/list/list.component';
+export * from './lib/list/list-container/list.component';
