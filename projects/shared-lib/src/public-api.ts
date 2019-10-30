0a77d8271799be272d39821c2cbd1614f9f9e5f2
Oleksandr Trokhymenko <Oleksandr_Trokhymenko@epam.com>
2019-10-30 15:34:00 +0200

    add  exports

--- projects/shared-lib/src/public-api.ts
+++ projects/shared-lib/src/public-api.ts
@@ -1,3 +1,12 @@
-export * from './lib/features/list/list.service';
+// exports modules
 export * from './lib/features/list/list.module';
 export * from './lib/shared/directives/border/border.module';
+
+// exports services
+export * from './lib/features/list/list.service';
+
+// exports components
+export * from './lib/features/list/container/list.component';
+
+// exports directives
+export * from './lib/shared/directives/border/border.directive';
